056c02c5bee19a86aa1cc76c5baebb9864478746
Michelle <micgep1015@gmail.com>
2023-04-08 22:38:25 -0700

    I added components

---
+++ personal_landing_page/src/contact/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './contact.css';
+
+function Contact() {
+  return (
+    <button>Contact Me</button>
+  )
+}
+
+export default Contact
---
+++ personal_landing_page/src/nav/Nav.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './nav.css'
+
+function Nav() {
+  return (
+    <>
+    <nav>
+    <div class = "firstTwoBalls">
+    <div class ="circle">
+    <p>Resume</p>
+    </div>
+    <div class = "circle">
+    <p>Portfolio</p>
+    </div>
+    </div>
+    <div class = "circle">
+    <p>Digital</p>
+    <p>Resume</p>
+    </div>
+  </nav>
+  </>
+  )
+}
+
+export default Nav
