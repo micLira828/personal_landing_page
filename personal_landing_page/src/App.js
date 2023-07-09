0dfa59fbdbcbd36db05469e997996af3f37881da
Michelle <micgep1015@gmail.com>
2023-07-09 11:30:28 -0700

    Moved it back

---
+++ personal_landing_page/src/App.js
@@ -0,0 +1,19 @@
+import Contact from './contact/Contact';
+import Nav from './nav/Nav';
+import Header from './header/Header';
+
+
+
+function App() {
+  return (
+    <>
+  <div class = "container">
+  <Header/>
+  <Contact/>
+  <Nav/>
+  </div>
+  </>
+  );
+}
+
+export default App;
